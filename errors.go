50b8c5cdea0879d520a644c52f36f6de654d305e
pchchv <ipchchv@gmail.com>
2023-03-16 14:15:07 +0300

    Implement a logCfg type

--- errors.go
+++ errors.go
@@ -1,5 +1,20 @@
 package web
 
+const (
+	// LogCfgDisableDebug is used to disable debug logs
+	LogCfgDisableDebug = logCfg("disable-debug")
+	// LogCfgDisableInfo is used to disable info logs
+	LogCfgDisableInfo = logCfg("disable-info")
+	// LogCfgDisableWarn is used to disable warning logs
+	LogCfgDisableWarn = logCfg("disable-warn")
+	// LogCfgDisableError is used to disable error logs
+	LogCfgDisableError = logCfg("disable-err")
+	// LogCfgDisableFatal is used to disable fatal logs
+	LogCfgDisableFatal = logCfg("disable-fatal")
+)
+
+type logCfg string
+
 // Logger defines all the logging methods to be implemented
 type Logger interface {
 	Debug(data ...interface{})
