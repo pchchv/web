7418e862ba7c4d4c2acaf920cca098c1cf5c2121
pchchv <ipchchv@gmail.com>
2023-03-16 13:57:18 +0300

    Add web package

---
+++ web.go
@@ -0,0 +1,12 @@
+/*
+The web package is a lightweight framework for building web applications.
+It has a multiplexer, a mechanism for connecting middleware, and its own context management.
+The main goal of web is to get as far away from the developer as possible,
+i.e. it doesn't force you to build your application according to any particular template,
+but just helps you make all the trivial things faster and easier.
+e.g.
+1. Getting named URI parameters.
+2. Multiplexer for regex-matching URIs and the like.
+3. Implementation of special application-level configurations or any similar objects into the request context as required.
+*/
+package web
