cfabd4d29e8e15dc77cdd680a631c6aafafc906e
pchchv <ipchchv@gmail.com>
2023-03-16 14:45:43 +0300

    Implement a Route type

--- route.go
+++ route.go
@@ -1,5 +1,41 @@
 package web
 
+import "net/http"
+
+// Route defines a route for each API
+type Route struct {
+	// Name is unique identifier for the route
+	Name string
+	// Method is the HTTP request method/type
+	Method string
+	// Pattern is the URI pattern to match
+	Pattern string
+	// TrailingSlash if set to true, the URI will be matched with or without
+	// a trailing slash. IMPORTANT: It does not redirect.
+	TrailingSlash bool
+
+	// FallThroughPostResponse if enabled will execute all the handlers even if a response was already sent to the client
+	FallThroughPostResponse bool
+
+	// Handlers is a slice of http.HandlerFunc which can be middlewares or anything else. Though only 1 of them will be allowed to respond to client.
+	// subsequent writes from the following handlers will be ignored
+	Handlers []http.HandlerFunc
+
+	hasWildcard bool
+	fragments   []uriFragment
+	paramsCount int
+
+	// skipMiddleware if true, middleware added using `router` will not be applied to this Route.
+	// This is used only when a Route is set using the RouteGroup, which can have its own set of middleware
+	skipMiddleware bool
+	// middlewareList is used at the last stage, i.e. right before starting the server
+	middlewarelist []Middleware
+
+	initialized bool
+
+	serve http.HandlerFunc
+}
+
 type uriFragment struct {
 	isVariable  bool
 	hasWildcard bool
