8689ce9eec2a8c1c10333d9fb3da3590a5a00d4b
pchchv <ipchchv@gmail.com>
2023-03-16 14:56:35 +0300

    Implement a Router type

--- router.go
+++ router.go
@@ -2,5 +2,31 @@ package web
 
 import "net/http"
 
+// Router is the HTTP router
+type Router struct {
+	optHandlers    []*Route
+	headHandlers   []*Route
+	getHandlers    []*Route
+	postHandlers   []*Route
+	putHandlers    []*Route
+	patchHandlers  []*Route
+	deleteHandlers []*Route
+	allHandlers    map[string][]*Route
+
+	// NotFound is the generic handler for 404 resource not found response
+	NotFound http.HandlerFunc
+
+	// NotImplemented is the generic handler for 501 method not implemented
+	NotImplemented http.HandlerFunc
+
+	// config has all the app config
+	config *Config
+
+	// httpServer is the server handler for the active HTTP server
+	httpServer *http.Server
+	// httpsServer is the server handler for the active HTTPS server
+	httpsServer *http.Server
+}
+
 // Middleware is the signature of WebGo's middleware
 type Middleware func(http.ResponseWriter, *http.Request, http.HandlerFunc)
