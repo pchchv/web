e8172f19fd246c20b37538a46f2847be0f769007
pchchv <ipchchv@gmail.com>
2023-03-19 10:46:16 +0300

    LoggerWithCfg test

---
+++ errors_test.go
@@ -0,0 +1,31 @@
+package web
+
+import "testing"
+
+func Test_loggerWithCfg(t *testing.T) {
+	t.Parallel()
+	cfgs := []logCfg{
+		LogCfgDisableDebug,
+		LogCfgDisableInfo,
+		LogCfgDisableWarn,
+		LogCfgDisableError,
+		LogCfgDisableFatal,
+	}
+	l := loggerWithCfg(nil, nil, cfgs...)
+
+	if l.debug != nil {
+		t.Errorf("expected debug to be nil, got %v", l.debug)
+	}
+	if l.err != nil {
+		t.Errorf("expected err to be nil, got %v", l.err)
+	}
+	if l.fatal != nil {
+		t.Errorf("expected fatal to be nil, got %v", l.fatal)
+	}
+	if l.info != nil {
+		t.Errorf("expected info to be nil, got %v", l.info)
+	}
+	if l.warn != nil {
+		t.Errorf("expected warn to be nil, got %v", l.warn)
+	}
+}
