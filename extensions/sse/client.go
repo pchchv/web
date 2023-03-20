92de9f1ebcf343eaa0f5b1c7a532db0b55c0eb57
pchchv <ipchchv@gmail.com>
2023-03-20 11:31:32 +0300

    Implement a Clients method and its methods

--- extensions/sse/client.go
+++ extensions/sse/client.go
@@ -3,6 +3,7 @@ package sse
 import (
 	"context"
 	"net/http"
+	"sync"
 )
 
 type Client struct {
@@ -10,4 +11,73 @@ type Client struct {
 	Msg            chan *Message
 	ResponseWriter http.ResponseWriter
 	Ctx            context.Context
+}
+
+type Clients struct {
+	clients   map[string]*Client
+	locker    sync.Mutex
+	MsgBuffer int
+}
+
+func (cs *Clients) New(ctx context.Context, w http.ResponseWriter, clientID string) (*Client, int) {
+	mchan := make(chan *Message, cs.MsgBuffer)
+	cli := &Client{
+		ID:             clientID,
+		Msg:            mchan,
+		ResponseWriter: w,
+		Ctx:            ctx,
+	}
+
+	cs.locker.Lock()
+	cs.clients[clientID] = cli
+	count := len(cs.clients)
+	cs.locker.Unlock()
+
+	return cli, count
+}
+
+func (cs *Clients) Range(f func(cli *Client)) {
+	cs.locker.Lock()
+	for clientID := range cs.clients {
+		f(cs.clients[clientID])
+	}
+	cs.locker.Unlock()
+}
+
+func (cs *Clients) Remove(clientID string) int {
+	cs.locker.Lock()
+	delete(cs.clients, clientID)
+	count := len(cs.clients)
+	cs.locker.Unlock()
+	return count
+}
+
+func (cs *Clients) Active() int {
+	cs.locker.Lock()
+	count := len(cs.clients)
+	cs.locker.Unlock()
+	return count
+}
+
+// MessageChannels returns a fragment of message channels of all clients,
+// which you can use to send messages simultaneously
+func (cs *Clients) Clients() []*Client {
+	idx := 0
+	cs.locker.Lock()
+	list := make([]*Client, len(cs.clients))
+	for clientID := range cs.clients {
+		cli := cs.clients[clientID]
+		list[idx] = cli
+		idx++
+	}
+	cs.locker.Unlock()
+	return list
+}
+
+func (cs *Clients) Client(clientID string) *Client {
+	cs.locker.Lock()
+	cli := cs.clients[clientID]
+	cs.locker.Unlock()
+
+	return cli
 }
