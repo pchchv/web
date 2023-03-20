b2d4dc2caef83734501f0c5e994fcd66b844cfe9
pchchv <ipchchv@gmail.com>
2023-03-20 10:34:53 +0300

    Add CORSEmptyconfig test

--- middleware/cors/cors_test.go
+++ middleware/cors/cors_test.go
@@ -1,7 +1,11 @@
 package cors
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 	"time"
 
 	"github.com/pchchv/web"
@@ -34,4 +38,86 @@ func setup(port string, routes []*web.Route) (*web.Router, error) {
 	router := web.NewRouter(cfg, routes...)
 
 	return router, nil
+}
+
+func TestCORSEmptyconfig(t *testing.T) {
+	port := "9696"
+	routes := getRoutes()
+	routes = append(routes, AddOptionsHandlers(nil)...)
+	router, err := setup(port, routes)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	router.Use(CORS(&Config{TimeoutSecs: 50}))
+	router.SetupMiddleware()
+
+	url := fmt.Sprintf("http://localhost:%s/hello", port)
+	w := httptest.NewRecorder()
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		url,
+		nil,
+	)
+
+	router.ServeHTTP(w, req)
+	body, _ := ioutil.ReadAll(w.Body)
+	str := string(body)
+	if str != "hello" {
+		t.Errorf(
+			"Expected body '%s', got '%s'",
+			"hello",
+			str,
+		)
+	}
+
+	if w.Header().Get(headerMethods) != defaultAllowMethods {
+		t.Errorf(
+			"Expected header %s to be '%s', got '%s'",
+			headerMethods,
+			defaultAllowMethods,
+			w.Header().Get(headerMethods),
+		)
+	}
+	if w.Header().Get(headerCreds) != "true" {
+		t.Errorf(
+			"Expected header %s to be 'true', got '%s'",
+			headerCreds,
+			w.Header().Get(headerCreds),
+		)
+	}
+	if w.Header().Get(headerAccessControlAge) != "50" {
+		t.Errorf(
+			"Expected '%s' to be '50', got '%s'",
+			headerAccessControlAge,
+			w.Header().Get(headerAccessControlAge),
+		)
+	}
+
+	if w.Header().Get(headerAllowHeaders) != allowHeaders {
+		t.Errorf(
+			"Expected '%s' to be '%s', got '%s'",
+			headerAllowHeaders,
+			allowHeaders,
+			w.Header().Get(headerAllowHeaders),
+		)
+	}
+
+	// check OPTIONS method
+	w = httptest.NewRecorder()
+	req = httptest.NewRequest(
+		http.MethodOptions,
+		url,
+		nil,
+	)
+	router.ServeHTTP(w, req)
+	body, _ = ioutil.ReadAll(w.Body)
+	str = string(body)
+	if str != "" {
+		t.Errorf(
+			"Expected empty body, got '%s'",
+			str,
+		)
+	}
 }
