e74ccef4b862e97cf5ab0ce557281ff371e22dbf
pchchv <ipchchv@gmail.com>
2023-03-21 10:19:17 +0300

    routegroupMiddleware and chain funcs

--- cmd/main.go
+++ cmd/main.go
@@ -7,5 +7,14 @@ import (
 
 var lastModified = time.Now().Format(http.TimeFormat)
 
+func chain(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("chained", "true")
+}
+
+func routegroupMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	w.Header().Add("routegroup", "true")
+	next(w, r)
+}
+
 func main() {
 }
